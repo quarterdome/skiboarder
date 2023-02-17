e51fce3bfe2563b9c5b523c5f816f3fbaa357fa8
Matt Nemenman <mattn@menem.com>
2023-02-16 17:25:20 -0800

    Initial

---
+++ app.py
@@ -0,0 +1,28 @@
+import gradio as gr
+from fastai.vision.all import *
+import skimage
+
+learn = load_learner('model.pkl')
+
+labels = learn.dls.vocab
+def predict(img):
+    # img = PILImage.create(img)
+    pred,pred_idx,probs = learn.predict(img)
+    return {labels[i]: float(probs[i]) for i in range(len(labels))}
+
+title = "Snowsport Classifier"
+description = "A snowsport classifier trained on bunch of images from internet via fastai."
+examples = ['skier.png','snowboarder.jpg']
+interpretation='default'
+enable_queue=True
+
+demo = gr.Interface(fn=predict,
+    inputs=gr.inputs.Image(shape=(512, 512)),
+    outputs=gr.outputs.Label(num_top_classes=3),
+    title=title,
+    description=description,
+    examples=examples,
+    interpretation=interpretation,
+    enable_queue=enable_queue).launch()
+
+demo.launch()
